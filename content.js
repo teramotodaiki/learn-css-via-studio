481378b6ee838f5068c46913657bf1ae5253eff7
teramotodaiki <i101323@gmail.com>
2020-08-20 11:41:08 +0900

    merge styles to 1 node (155314 chars formatted)

--- content.js
+++ content.js
@@ -1,3 +1,5 @@
+const __id = 'learn-css-via-studio-button';
+
 window.addEventListener('load', start);
 function start() {
   const studioBanner = document.querySelector('div.studio__banner');
@@ -6,28 +8,72 @@ function start() {
     return;
   }
   const exportButton = document.createElement('div');
-  exportButton.id = 'learn-css-via-studio-button';
+  exportButton.id = __id;
   studioBanner.parentElement.appendChild(exportButton);
   const rect = studioBanner.getBoundingClientRect();
   exportButton.style.left = rect.left + 'px';
   exportButton.style.bottom = innerHeight - rect.top + 16 + 'px';
   exportButton.textContent = 'CODE';
-  exportButton.addEventListener('click', code, { passive: true });
+  exportButton.addEventListener(
+    'click',
+    () => {
+      const html = getHTML();
+      const file = new Blob([html], { type: 'text/html' });
+      const url = URL.createObjectURL(file);
+      window.open(url, '_blank'); // TODO: Make it downloadable, if smaller than 65536 bytes
+      URL.revokeObjectURL(url);
+    },
+    { passive: true }
+  );
 }
 
-function code() {
+function getHTML() {
+  const styleSheets = Array.from(document.styleSheets);
+  const css = styleSheets.reduce((content, styleSheet) => {
+    const { tagName } = styleSheet.ownerNode;
+    if (tagName === 'STYLE') {
+      for (const rule of styleSheet.cssRules) {
+        content += rule.cssText;
+      }
+    }
+    return content;
+  }, '');
+  const head = styleSheets.reduce((content, styleSheet) => {
+    if (styleSheet.ownerNode.tagName === 'LINK') {
+      content += styleSheet.ownerNode.outerHTML;
+    }
+    return content;
+  }, '');
+
+  const root = document.querySelector('.StudioCanvas');
+  const body = Array.from(root.children).reduce((content, node) => {
+    if (
+      node.className !== 'publish-studio-style' &&
+      node.className !== 'studio__banner' &&
+      node.id !== __id
+    ) {
+      content += node.outerHTML;
+    }
+    return content;
+  }, '');
+
   const html = `<!DOCTYPE html>
-    <html lang="en">
+    <html>
     <head>
-    ${document.head.innerHTML}
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>${document.title}</title>
+    ${head}
+    <style>
+    ${css}
+    </style>
     </head>
     <body>
-    ${document.body.innerHTML}
+    <div class="StudioCanvas">
+    ${body}
+    </div>
     </body>
     </html>`;
 
-  const file = new Blob([html], { type: 'text/html' });
-  const url = URL.createObjectURL(file);
-  window.open(url, '_blank');
-  URL.revokeObjectURL(url);
+  return html;
 }
