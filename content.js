0234a689fb7df948914cf475608239f1cdafef69
teramotodaiki <i101323@gmail.com>
2020-08-20 22:59:54 +0900

    reduce attributes (61467 chars formatted)

--- content.js
+++ content.js
@@ -69,6 +69,9 @@ function getHTML() {
   const selectorToRulesMap = new Map([['', {}]]); // type hint
   selectorToRulesMap.clear();
 
+  const styleDeclarationMap = new Map([['', {}]]); // type hint
+  styleDeclarationMap.clear();
+
   /**
    * Find element which is queried that selector
    * @param {CSSRule} rule
@@ -76,9 +79,16 @@ function getHTML() {
   function getOptimizedCssText(rule) {
     if (rule.type === CSSRule.STYLE_RULE) {
       const selector = extractPseudoElementFromSelectorText(rule.selectorText);
-      if (selector && root.querySelector(selector)) {
+      const node = selector && root.querySelector(selector);
+      if (node) {
         // to omit same properties assigned to same selectors in different media queries
         selectorToRulesMap.set(rule.selectorText, rule.style); // cache
+        const result = /^\.sd\[(data-s-.*)\]$/.exec(rule.selectorText);
+        if (result) {
+          const decls = styleDeclarationMap.get(result[1]) || {};
+          decls[''] = rule.style.cssText;
+          styleDeclarationMap.set(result[1], decls);
+        }
         return rule.cssText;
       }
       return '';
@@ -98,6 +108,13 @@ function getHTML() {
           }
           if (r.style.length === 0) {
             rule.deleteRule(index); // remove empty CSSRule
+          } else {
+            const result = /^\.sd\[(data-s-.*)\]$/.exec(r.selectorText);
+            if (result) {
+              const decls = styleDeclarationMap.get(result[1]) || {};
+              decls[rule.conditionText] = r.style.cssText;
+              styleDeclarationMap.set(result[1], decls);
+            }
           }
         }
       }
@@ -115,7 +132,7 @@ function getHTML() {
     throw new Error('Not implemented rule type: ' + rule.type);
   }
 
-  const css = styleSheets.reduce((content, styleSheet) => {
+  let css = styleSheets.reduce((content, styleSheet) => {
     const { tagName } = styleSheet.ownerNode;
     if (tagName === 'STYLE') {
       for (const rule of styleSheet.cssRules) {
@@ -124,6 +141,44 @@ function getHTML() {
     }
     return content;
   }, '');
+
+  // replace uuid attribute into short className
+  const attrToShortNameMap = new Map([['', '']]); // type hint
+  attrToShortNameMap.clear();
+  const hashMap = new Map([['', '']]); // type hint
+  hashMap.clear();
+  for (const [attr, decls] of styleDeclarationMap.entries()) {
+    const serialized = JSON.stringify(decls);
+    const className = hashMap.get(serialized);
+    if (className === undefined) {
+      const newOne = `c${hashMap.size + 1}`;
+      attrToShortNameMap.set(attr, newOne);
+      hashMap.set(serialized, newOne);
+    } else {
+      attrToShortNameMap.set(attr, className);
+    }
+  }
+
+  for (const [attr, cn] of attrToShortNameMap.entries()) {
+    css = replaceAll(css, `.sd[${attr}]`, `.sd.${cn}`);
+  }
+  /**
+   *
+   * @param {Element} element
+   */
+  function replaceAttr(element) {
+    for (const attr of Array.from(element.getAttributeNames())) {
+      const cn = attrToShortNameMap.get(attr);
+      if (cn) {
+        element.removeAttribute(attr);
+        element.classList.add(cn);
+      }
+    }
+    for (const child of Array.from(element.children)) {
+      replaceAttr(child);
+    }
+  }
+  replaceAttr(root);
 
   const body = root.outerHTML;
 
@@ -158,3 +213,19 @@ function extractPseudoElementFromSelectorText(selectorText) {
     .filter((s) => s)
     .join(',');
 }
+
+/**
+ * String.prototype.replaceAll
+ * @param {String} str
+ * @param {String} from
+ * @param {String} to
+ */
+function replaceAll(str, from, to) {
+  return str.split(from).join(to);
+}
+
+/**
+ *
+ * @param {CSSStyleDeclaration} style
+ */
+function serializeStyle(style) {}
